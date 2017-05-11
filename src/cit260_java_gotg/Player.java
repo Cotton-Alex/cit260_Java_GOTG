b608a644cb1ada8b4196562612b6d0d3d6f33c4f
Mike Minassian <minassbond@gmail.com>
2017-05-11 16:05:25 -0400

    Worked on player class

--- src/cit260_java_gotg/Player.java
+++ src/cit260_java_gotg/Player.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Player class as shown in reading
  */
 package cit260_java_gotg;
 
