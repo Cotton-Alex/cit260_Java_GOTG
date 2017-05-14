73626b548d2726612531f0cfd4516876594f97c1
Mike Minassian <minassbond@gmail.com>
2017-05-13 20:59:52 -0400

    Worked on player class

---
+++ src/cit260_java_gotg/Actor.java
@@ -0,0 +1,21 @@
+/*
+ *The Actor Class
+ */
+package cit260_java_gotg;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Mike Minassian
+ */
+
+public class Actor implements Serializable{
+    
+//class instance variables
+    private String name;
+public Actor() {
+    
+}
+}
