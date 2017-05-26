40d990ab97b1ccc95e62b8fe7d9b0f5f835f5a73
Mike Minassian <minassbond@gmail.com>
2017-05-25 22:31:11 -0400

    Created Inifinity Stone, Actor, and Inventory Classes

---
+++ src/cit260_java_gotg/fuelNeededControlTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260_java_gotg;
+
+/**
+ *
+ * @author Mike Minassian
+ */
+public class fuelNeededControlTest {
+    
+    public void main(String[] args){
+        int quadrantCount = 5;
+        
+        int sectorCount = 6;
+        
+        FuelNeeded fuelNeeded = new FuelNeeded();
+        
+        int fuelNeeded = fuelneeded.getFuelNeeded(quadrantCount, sectorCount);
+    }
+}
