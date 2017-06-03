de17cb567114a36e28875f406d62305157280a9a
Alex <cot15011@byui.edu>
2017-06-02 22:16:17 -0700

    saved up to page 32. taking a break for the night

--- src/GOTG/GOTG.java
+++ src/GOTG/GOTG.java
@@ -133,12 +133,8 @@ public class GOTG {
         return player;
     }      
 
-    public static void setPlayer(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-//    public static void setPlayer(Player player);
-//        GOTG.player = player;
+    public static void setPlayer(Player player);
+        GOTG.player = player;
 
     private static class player {
 
