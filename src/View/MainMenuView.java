ab0cb0eb614233ebcd6be27b56c074e31fe30307
Mike Minassian <minassbond@gmail.com>
2017-06-02 22:27:47 -0400

    Created Inifinity Stone, Actor, and Inventory Classes

--- src/View/MainMenuView.java
+++ src/View/MainMenuView.java
@@ -10,6 +10,21 @@ package View;
  * @author alexcotton
  */
 public class MainMenuView {
+    
+    private String menu;
+    
+    public MainMenuView(){
+        this.menu = "\n"
+                   +"\n----------------------------------"
+                   +"\n|  Main Menu                     |"
+                   +"\n----------------------------------"
+                   +"\nN - Start new game"
+                   +"\nG - Get and start saved game"
+                   +"\nH - Get help on how to play"
+                   +"\nS - Save game"
+                   +"\nQ - Quit"
+                   +"\n----------------------------------";
+    }
 
     void displayMainMenuView() {
         System.out.println("\ndisplayMenu() function called ***");
