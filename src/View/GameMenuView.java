63ed885fb22b2dfa734b5d87abfe6dd6e13d76a5
Mike Minassian <minassbond@gmail.com>
2017-06-17 17:54:58 -0400

    Updated Game Menu

--- src/View/GameMenuView.java
+++ src/View/GameMenuView.java
@@ -11,12 +11,10 @@ import java.util.Scanner;
  *
  * @author Mike Minassian
  */
-public class GameMenuView {
-    private String menu;
-    private String promptMessage;
+public class GameMenuView extends View {
 
     public GameMenuView() {
-        this.menu = "\n" + "\n----------------------------------"
+        super("\n" + "\n----------------------------------"
                          + "\n|  Game Menu                     |"
                          + "\n----------------------------------"
                          + "\nM - Game Map"
@@ -25,44 +23,9 @@ public class GameMenuView {
                          + "\nT - Time and Fuel Remaining"
                          + "\nS - Save game"
                          + "\nQ - Main Menu"
-                         + "\n----------------------------------";
+                         + "\n----------------------------------");
     }
-
-    public void displayGameMenuView() {
-        boolean done = false;    // set flag to not done
-        do {
-            // prompt for and get players name
-            String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("Q"))      // user want to quit
-                return;                                    // exit the game
-
-            // do the requested action and display the next view
-            done = this.doAction(menuOption);
-        } while (!done);
-    }
-
-    public String getMenuOption() {
-        Scanner keyboard = new Scanner(System.in);    // get infile for keyboard
-        String  value    = "";                        // value to be returned
-        boolean valid    = false;                     // initialize to not valid
-
-        while (!valid) {
-            System.out.println("\n" + this.menu);
-            value = keyboard.nextLine();    // get next line typed on keyboard
-            value = value.trim();           // trim off leading and trailing blanks
-
-            if (value.length() < 1) {
-                System.out.println("\nInvalid value: value can not be blank");
-
-                continue;
-            }
-
-            break;                          // end the loop
-        }
-
-        return value;    // return the value entered
-    }
-    
+   
     public boolean doAction(String choice){
         
         choice = choice.toUpperCase(); // convert choice to upper case
