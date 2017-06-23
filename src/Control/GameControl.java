4977352235bb896528d3f4b86f6e6205a846cf4e
Justin <Justin@LAPTOP-6OEOBT70>
2017-06-23 11:08:01 -0600

    5/13/17 Completed my assignment and fixed some problems in some of the classes

--- src/Control/GameControl.java
+++ src/Control/GameControl.java
@@ -35,9 +35,59 @@ public class GameControl {
         MapControl.moveActorsToStartingLocation(map);
     }
     
+    public enum Item {
+        mindStone,
+        tesseract,
+        aether,
+        orb,
+        soulStone,
+        timeStone;
+    }
+    
     public static Inventory[] createInventory(){
-        System.out.println("*** called createInventory() in GameControl ***");
-        return null;
+       
+        //array(list) for inventory
+        Inventory[] inventory = new Inventory[5];
+        
+        Inventory mindStone = new Inventory();
+        mindStone.setInventoryType("Mind Stone");
+        mindStone.setQuantityInStock(0);
+        mindStone.setRequiredAmount(0);
+        inventory[Item.mindStone.ordinal()] = mindStone;
+        
+               Inventory tesseract = new Inventory();
+        tesseract.setInventoryType("Tesseract");
+        tesseract.setQuantityInStock(0);
+        tesseract.setRequiredAmount(0);
+        inventory[Item.tesseract.ordinal()] = tesseract;
+        
+               Inventory aether = new Inventory();
+        aether.setInventoryType("Aether");
+        aether.setQuantityInStock(0);
+        aether.setRequiredAmount(0);
+        inventory[Item.aether.ordinal()] = aether;
+        
+               Inventory orb = new Inventory();
+        orb.setInventoryType("Orb");
+        orb.setQuantityInStock(0);
+        orb.setRequiredAmount(0);
+        inventory[Item.orb.ordinal()] = orb;
+        
+               Inventory soulStone = new Inventory();
+        soulStone.setInventoryType("Soul Stone");
+        soulStone.setQuantityInStock(0);
+        soulStone.setRequiredAmount(0);
+        inventory[Item.soulStone.ordinal()] = soulStone;
+        
+               Inventory timeStone = new Inventory();
+        timeStone.setInventoryType("Time Stone");
+        timeStone.setQuantityInStock(0);
+        timeStone.setRequiredAmount(0);
+        inventory[Item.timeStone.ordinal()] = timeStone;
+        
+        
+        
+        return inventory;
     }
 
     public static Player createPlayer(String name) {
