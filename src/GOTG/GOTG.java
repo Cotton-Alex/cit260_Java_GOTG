e2cf1eb3a7a13bc2da9ffaabb76642aa41323945
Mike Minassian <minassbond@gmail.com>
2017-07-05 23:01:57 -0400

    Worked Through Pg 6 of team assignment

--- src/GOTG/GOTG.java
+++ src/GOTG/GOTG.java
@@ -6,52 +6,96 @@ package GOTG;
 import Model.Player;
 import Model.Game;
 import View.StartProgramView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author alexcotton
  */
 public class GOTG {
-    
+
     /**
      * @param args the command line arguments
      */
     private static Game currentGame = null;
-    
     private static Player player = null;
-    
+
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+
 //********************************************************************************************************************
 //     01 MAIN - the start
 //********************************************************************************************************************
-    
     public static void main(String[] args) {
-       StartProgramView startProgramView = new StartProgramView();
-       try {
-       startProgramView.displayStartProgramView();
-       } catch (Throwable te) {
-           System.out.println(te.getMessage());
-           te.printStackTrace();
-           startProgramView.displayStartProgramView();
-       }
-       
+        StartProgramView startProgramView = new StartProgramView();
+        try {
+            GOTG.inFile = new BufferedReader(new InputStreamReader(System.in));
+            GOTG.outFile = new PrintWriter(System.out, true);
+
+            //StartProgramView startProgramView = new StartProgramView();
+            startProgramView.displayStartProgramView();
+            return;
+        } catch (Throwable te) {
+            System.out.println(te.getMessage());
+            te.printStackTrace();
+        }
+        
+        finally{
+            try {
+                if (GOTG.inFile != null)
+                    GOTG.inFile.close();
+                
+                if (GOTG.outFile != null)
+                    GOTG.outFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error closing files");
+                return;
+            }
+            
+        }
+        startProgramView.displayStartProgramView();
     }
 
     public static Game getCurrentGame() {
         return currentGame;
     }
+
     public static void setCurrentGame(Game currentGame) {
-        GOTG.currentGame= currentGame;
-    }   
+        GOTG.currentGame = currentGame;
+    }
+
     public static Player getPlayer() {
         return player;
-    }      
+    }
+
     public static void setPlayer(Player player) {
         GOTG.player = player;
     }
+
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        GOTG.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        GOTG.inFile = inFile;
+    }
+
     private static class player {
 
         public player() {
         }
     }
 }
-
